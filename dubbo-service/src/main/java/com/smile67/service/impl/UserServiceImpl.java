bf1911452bb4e659909f5c78fe5871a334320d12
smile67 <1639316332@qq.com>
2024-07-31 20:57:11 +0800

    feat: 服务提供者改造

--- dubbo-service/src/main/java/com/smile67/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/smile67/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.smile67.service.impl;
 
 import com.smile67.service.UserService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
-@Service
+
+//@Service// 将该类的对象创建出来，放到Spring的IOC容器中 bean定义
+@Service // 导入dubbo的注解  将这个类提供的方法（服务）对外发布。将访问的ip端口路径注册到 注册中心
 public class UserServiceImpl implements UserService {
     @Override
     public String sayHello() {
